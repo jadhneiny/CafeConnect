96403d5b7c5fe98678030ffc82e9d7d8e737336d
jbh07 <jadhneiny123@gmail.com>
2024-02-27 14:13:42 +0200

    completed front end of login/signup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-// src/App.js
+ import React from "react";
+ import * as Components from './Components';
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage'; // Import the HomePage component
-import SignUp from './SignUp';
-import Login from './Login';
-import './App.css';
+ function App() {
+     const [signIn, toggle] = React.useState(true);
+      return(
+          <Components.Container>
+              <Components.SignUpContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Create Account</Components.Title>
+                      <Components.Input type='text' placeholder='Name' />
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Button>Sign Up</Components.Button>
+                  </Components.Form>
+              </Components.SignUpContainer>
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} /> {/* Add this line */}
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
-  );
-}
+              <Components.SignInContainer signinIn={signIn}>
+                   <Components.Form>
+                       <Components.Title>Sign in</Components.Title>
+                       <Components.Input type='email' placeholder='Email' />
+                       <Components.Input type='password' placeholder='Password' />
+                       <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                       <Components.Button>Sign In</Components.Button>
+                   </Components.Form>
+              </Components.SignInContainer>
 
-export default App;
+              <Components.OverlayContainer signinIn={signIn}>
+                  <Components.Overlay signinIn={signIn}>
+
+                  <Components.LeftOverlayPanel signinIn={signIn}>
+                      <Components.Title>CafeConnect</Components.Title>
+                      <Components.Paragraph>
+                          Check the availability of your favorite items in your local cafeteria!
+                      </Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(true)}>
+                          Sign In
+                      </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title>CafeConnect</Components.Title>
+                        <Components.Paragraph>
+                            Sign Up to view the menu of your local cafeteria!
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sign Up
+                            </Components.GhostButton> 
+                      </Components.RightOverlayPanel>
+  
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+
+          </Components.Container>
+      )
+ }
+
+ export default App;
