1e67da8be4a2b3c627a8331dea8309302e710434
hss30Hadi <144145406+hss30Hadi@users.noreply.github.com>
2024-03-11 10:16:45 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import WeekDaysPage from './Component/WeekDaysPage';
+
+function App() {
+  return( 
+  <div className="App"><WeekDaysPage />
+    </div>
+    )
+}
+
+export default App;
---
+++ WeekDaysData.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+export const WeekDaysData = [
+    {
+        title: "CafeConnect",
+        icon:<FastfoodIcon />
+    },
+    {
+        title: "Monday",
+        icon:<ChevronRightIcon />,
+        link: "/MondayMenu"
+    },
+    {
+        title: "Tuesday",
+        icon:<ChevronRightIcon />,
+        link: "/TuesdayMenu"
+    },
+    {
+        title: "Wednesday",
+        icon:<ChevronRightIcon />,
+        link: "/WednesdayMenu"
+    },
+    {
+        title: "Thursday",
+        icon:<ChevronRightIcon />,
+        link: "/ThursdayMenu"
+    },
+    {
+        title: "Friday",
+        icon:<ChevronRightIcon />,
+        link: "/FridayMenu"
+    },
+    {
+        title: "Saturday",
+        icon:<ChevronRightIcon />,
+        link: "/SaturdayMenu"
+    },
+    {
+        title: "Sunday",
+        icon:<ChevronRightIcon />,
+        link: "/SundayMenu"
+    }
+];
