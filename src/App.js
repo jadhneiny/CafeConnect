e2dbd84e7811de140915dc5dca15a34df0824614
KamalKazzaz <150523806+KamalKazzaz@users.noreply.github.com>
2024-03-12 20:38:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -97,67 +97,71 @@ function App() {
   }
 
   return (
-    <Components.Container>
-      <Components.SignUpContainer signinIn={signIn}>
-        <Components.Form onSubmit={handleSignUp}>
-          {" "}
-          <Components.Title>Create Account</Components.Title>
-          <Components.Input name="name" type="text" placeholder="Name" />
-          <Components.Input name="email" type="email" placeholder="Email" />
-          <Components.Input
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-          <Components.Button type="submit">Sign Up</Components.Button>
-        </Components.Form>
-      </Components.SignUpContainer>
+  <Components.Container>
+    {/* Sign-up error message */}
+    {signUpError && (
+      <Components.ErrorParagraph className="error-message">
+        {signUpError}
+      </Components.ErrorParagraph>
+    )}
 
-      <Components.SignInContainer signinIn={signIn}>
-        <Components.Form onSubmit={handleSignIn}>
-          {" "}
-          <Components.Title>Sign in</Components.Title>
-          <Components.Input
-            name="email"
-            type="email"
-            placeholder="Email"
-          />{" "}
-          <Components.Input
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-          <Components.Anchor href="#">Forgot your password?</Components.Anchor>
-          <Components.Button type="submit">Sign In</Components.Button>{" "}
-        </Components.Form>
-      </Components.SignInContainer>
+    <Components.SignUpContainer signinIn={signIn}>
+      <Components.Form onSubmit={handleSignUp}>
+        <Components.Title>Create Account</Components.Title>
+        <Components.Input name="name" type="text" placeholder="Name" />
+        <Components.Input name="email" type="email" placeholder="Email" />
+        <Components.Input
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <Components.Button type="submit">Sign Up</Components.Button>
+      </Components.Form>
+    </Components.SignUpContainer>
 
-      <Components.OverlayContainer signinIn={signIn}>
-        <Components.Overlay signinIn={signIn}>
-          <Components.LeftOverlayPanel signinIn={signIn}>
-            <Components.Title>CafeConnect</Components.Title>
-            <Components.Paragraph>
-              Check the availability of your favorite items in your local
-              cafeteria!
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(true)}>
-              Sign In
-            </Components.GhostButton>
-          </Components.LeftOverlayPanel>
+    {/* Sign-in error message */}
+    {signInError && (
+      <Components.ErrorParagraph className="error-message">
+        {signInError}
+      </Components.ErrorParagraph>
+    )}
 
-          <Components.RightOverlayPanel signinIn={signIn}>
-            <Components.Title>CafeConnect</Components.Title>
-            <Components.Paragraph>
-              Sign Up to view the menu of your local cafeteria!
-            </Components.Paragraph>
-            <Components.GhostButton onClick={() => toggle(false)}>
-              Sign Up
-            </Components.GhostButton>
-          </Components.RightOverlayPanel>
-        </Components.Overlay>
-      </Components.OverlayContainer>
-    </Components.Container>
-  );
+    <Components.SignInContainer signinIn={signIn}>
+      <Components.Form onSubmit={handleSignIn}>
+        <Components.Title>Sign in</Components.Title>
+        <Components.Input name="email" type="email" placeholder="Email" />
+        <Components.Input
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <Components.Anchor href="#">Forgot your password?</Components.Anchor>
+        <Components.Button type="submit">Sign In</Components.Button>
+      </Components.Form>
+    </Components.SignInContainer>
+
+    {/* Overlay */}
+    <Components.OverlayContainer signinIn={signIn}>
+      <Components.Overlay signinIn={signIn}>
+        <Components.LeftOverlayPanel signinIn={signIn}>
+          <Components.Title>CafeConnect</Components.Title>
+          <Components.Paragraph>
+            Check the availability of your favorite items in your local cafeteria!
+          </Components.Paragraph>
+          <Components.GhostButton onClick={() => toggle(true)}>Sign In</Components.GhostButton>
+        </Components.LeftOverlayPanel>
+
+        <Components.RightOverlayPanel signinIn={signIn}>
+          <Components.Title>CafeConnect</Components.Title>
+          <Components.Paragraph>
+            Sign Up to view the menu of your local cafeteria!
+          </Components.Paragraph>
+          <Components.GhostButton onClick={() => toggle(false)}>Sign Up</Components.GhostButton>
+        </Components.RightOverlayPanel>
+      </Components.Overlay>
+    </Components.OverlayContainer>
+  </Components.Container>
+);
 }
 
 function AppWrapper() {
