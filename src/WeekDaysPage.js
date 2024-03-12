ec28e0f2a70ba5d661cb7287dc931decdf79604b
jadhneiny <jadhneiny123@gmail.com>
2024-03-12 23:08:45 +0200

    last min fixes, front end

--- src/WeekDaysPage.js
+++ src/WeekDaysPage.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import './App.css';
-import { WeekDaysData } from './WeekDaysData';
+import React from "react";
+import "./App.css";
+import { WeekDaysData } from "./WeekDaysData";
 
 function WeekDaysPage() {
   return (
     <div className="WeekDaysPage">
-        <ul className="WeekDaysList">
-        {WeekDaysData.map((val,key) => {
-           return(
-            <li key={key}
-            className="row"
-            id={window.location.pathname === val.link? "active" : ""}
-            onClick={() => {
+      <ul className="WeekDaysList">
+        {WeekDaysData.map((val, key) => {
+          return (
+            <li
+              key={key}
+              className="row"
+              id={window.location.pathname === val.link ? "active" : ""}
+              onClick={() => {
                 window.location.pathname = val.link;
-                }}
-            >   <div id="icon">{val.icon}</div>
-                <div id="title">{val.title}</div>
+              }}
+            >
+              {" "}
+              <div id="icon">{val.icon}</div>
+              <div id="title">{val.title}</div>
             </li>
           );
         })}
-        </ul>
+      </ul>
     </div>
   );
 }
